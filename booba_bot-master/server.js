1b1993318569cde3fee947e2889c8362717051bf
sevrai <severin.gouge@gmail.com>
2017-07-19 17:01:45 +0100

    ms to secondes

--- booba_bot-master/server.js
+++ booba_bot-master/server.js
@@ -200,7 +200,7 @@ app.get('/larbi/:time1', function(req, res){
 var date = new Date();
 var time = date.getTime();
 var time1 = parseInt(req.params.time1.replace(/\+/gi, " "));
-var diff = time1-time;
+var diff = (time-time1)/1000;
 res.setHeader('Content-Type', 'text/plain');
 res.status(200);
 
