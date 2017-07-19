84d230d322523bb9c87b92fb143a7a87f2f6017e
sevrai <severin.gouge@gmail.com>
2017-07-19 16:54:30 +0100

    boyscout

--- booba_bot-master/server.js
+++ booba_bot-master/server.js
@@ -199,7 +199,7 @@ request.post({
 app.get('/larbi/:time', function(req, res){
 var date = new Date();
 var time = date.getTime();
-var time1 = int(req.params.blaz.replace(/\+/gi, " "));
+var time1 = parseInt(req.params.blaz.replace(/\+/gi, " "));
 var diff = time1-time;
 res.setHeader('Content-Type', 'text/plain');
 res.status(200);
